94b1be5cf8f14aa1a0886c2707cd552ef39041a9
shahanalata <shahanalata84@gmail.com>
2022-12-31 21:08:14 -0500

    Drag & Drop

---
+++ src/main/java/DragDrop.java
@@ -0,0 +1,32 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.List;
+
+public class DragDrop {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://jqueryui.com/droppable/");
+
+        WebElement frame = driver.findElement(By.tagName("iframe"));//tagname start with iframe
+        List<WebElement> totalFrame = driver.findElements(By.tagName("iframe"));//collection--use list bcz(iframe) follow index number
+        totalFrame.size();
+        System.out.println("total frame "+totalFrame.size());
+        driver.switchTo().frame(frame);
+
+        WebElement Source  = driver.findElement(By.xpath("//*[@id='draggable']"));//which we drag(start from here)
+        WebElement target = driver.findElement(By.id("droppable"));//where we drop
+        Actions ac = new Actions(driver);
+        ac.dragAndDrop(Source, target).build().perform();//
+        driver.switchTo().defaultContent();
+        driver.findElement(By.xpath("//*[text()='Draggable']")).click();
+
+        Thread.sleep(3000l);
+        driver.quit();
+    }
+}
