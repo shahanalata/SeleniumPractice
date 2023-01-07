f1a6c2ea9982eafd45601d95fa4a228a503a7680
shahanalata <shahanalata84@gmail.com>
2023-01-07 11:11:50 -0500

    Flipcart

--- src/main/java/Selenium/FlipCart.java
+++ src/main/java/Selenium/FlipCart.java
@@ -20,13 +20,12 @@ public class FlipCart {
        //driver.findElement(By.xpath("//button[@class='_2KpZ6l _2doB4z']")).click();
         driver.findElement(By.cssSelector("._3704LK")).sendKeys("iphone14");
         Thread.sleep(3000);
-       List<WebElement> allNames = driver.findElements(By.xpath("//ul[@class='col-12-12 _1MRYA1 _38UFBk']"));
+       List<WebElement> allNames = driver.findElements(By.xpath("//div[contains(@class,'lrtEPN _17d0yO')]"));
         System.out.println(allNames.size());
         for (int i = 0; i < allNames.size(); i++) {
             System.out.println( allNames.get(i).getText());
             String Names = allNames.get(i).getText();
-            Thread.sleep(2000l);
-            if (Names.equalsIgnoreCase("iphone14cover")) {
+            if (Names.equalsIgnoreCase("iphone14 cover")) {
                 allNames.get(i).click();
             }
         }
@@ -34,3 +33,4 @@ public class FlipCart {
         driver.quit();
     }
 }
+////a[@class='_3izBDY']//div[@class='lrtEPN _17d0yO']//span
